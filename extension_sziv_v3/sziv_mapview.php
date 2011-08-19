8bd3d1fe27ac0ad1a120a7b12c0adfd32dfd7f9f
Peter Kerepesi <kerepesi.peter@ustream.tv>
2011-08-19 07:22:13 +0200

    [UPG] added mapview & userreg connector module

---
+++ extension_sziv_v3/sziv_mapview.php
@@ -0,0 +1,10 @@
+<?php
+
+function sziv_mapview_main() {
+	$content = '';
+	$content .= 'sziv_mapview_works!';
+	
+	return $content;
+}
+
+?>
---
+++ extension_sziv_v3/sziv_userreg.php
@@ -0,0 +1,10 @@
+<?php
+
+
+function sziv_userreg_main() {
+	$content = '';
+	$content .= 'sziv_userreg_works!';	
+	return $content;
+}
+
+?>
